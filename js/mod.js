2b6a366769dcfdb5ec8a354f627b17f69e579b1d
Chatterpox <mjohns1007@gmail.com>
2024-04-07 12:28:42 -0500

    Update mod.js
    Made small changes to just the name, id and author info, no game changes yet.

--- js/mod.js
+++ js/mod.js
@@ -1,7 +1,7 @@
 let modInfo = {
-	name: "The ??? Tree",
-	id: "mymod",
-	author: "nobody",
+	name: "Eternum Tree",
+	id: "eternumchatterpox",
+	author: "Chatterpox",
 	pointsName: "points",
 	modFiles: ["layers.js", "tree.js"],
 
